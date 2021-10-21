7aa2597cf40c250be3374f2881ce1009f6fcf681
Rodrigo Pazos <ropa1998@gmail.com>
2021-10-21 11:45:53 -0300

    solved env variable

--- src/utils/Server.js
+++ src/utils/Server.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: "http://localhost:8080"
-    // baseURL: "https://sig-back.herokuapp.com/"
+    // baseURL: "http://localhost:8080"
+    baseURL: "https://sig-back.herokuapp.com/"
 });
 
 axiosInstance.interceptors.request.use(function (config) {
